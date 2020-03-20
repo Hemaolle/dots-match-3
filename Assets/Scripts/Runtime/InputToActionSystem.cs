bbb394fc112b559f6ee453e377dc322ef9cdd4a3
Oskari Leppäaho <oskari.leppaaho@rovio.com>
2020-03-20 15:18:24 +0200

    Debug log when the user clicks on hexagon

--- Assets/Scripts/Runtime/InputToActionSystem.cs
+++ Assets/Scripts/Runtime/InputToActionSystem.cs
@@ -2,6 +2,7 @@ using Unity.Entities;
 using Unity.Jobs;
 using Unity.Collections;
 using Unity.Mathematics;
+using Unity.Tiny;
 using Unity.Tiny.Rendering;
 using Unity.U2D.Entities.Physics;
 
@@ -20,30 +21,39 @@ namespace Unity.TinyGems
 
             m_CameraQuery = GetEntityQuery(typeof(CameraMatrices));
             
-            RequireSingletonForUpdate<ActiveInput>();
+            //RequireSingletonForUpdate<ActiveInput>();
         }
 
         protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
             var input = World.GetExistingSystem<Tiny.Input.InputSystem>();
             var physicsWorld = World.GetExistingSystem<PhysicsWorldSystem>().PhysicsWorld;
-
-            if (InputUtil.GetInputDown(input))
-            {
-                m_StartPosition = CameraUtil.ScreenPointToViewportPoint(EntityManager, InputUtil.GetInputPosition(input));   
-            }
-            else if (InputUtil.GetInputUp(input))
+            
+            if (InputUtil.GetInputUp(input))
             {
                 var inputPos = CameraUtil.ScreenPointToViewportPoint(EntityManager, InputUtil.GetInputPosition(input));
-                var inputDelta = inputPos - m_StartPosition;
+                
+                Debug.Log("inputPos" + inputPos);
                 
                 var cameraMatrices = m_CameraQuery.ToComponentDataArray<CameraMatrices>(Allocator.TempJob);
-                var resultPos = CameraUtil.ViewPortPointToNearClipPoint(cameraMatrices[0], m_StartPosition);
-
-                var activeInput = GetSingleton<ActiveInput>();
-                activeInput.Value = GetSwapInputFromWorldPos(ref physicsWorld, resultPos, inputDelta);
+                var resultPos = CameraUtil.ViewPortPointToNearClipPoint(cameraMatrices[0], inputPos);
                 
-                SetSingleton(activeInput);
+                var pointInput = new OverlapPointInput()
+                {
+                    Position = resultPos,
+                    Filter = CollisionFilter.Default
+                };
+                
+                if (physicsWorld.OverlapPoint(pointInput, out var overlapPointHit))
+                {
+                    var body = physicsWorld.AllBodies[overlapPointHit.PhysicsBodyIndex];
+                    Debug.Log("input hit physicsbody " + body.Entity.Index);
+                }
+
+//                var activeInput = GetSingleton<ActiveInput>();
+//                activeInput.Value = GetSwapInputFromWorldPos(ref physicsWorld, resultPos, inputDelta);
+                
+//                SetSingleton(activeInput);
 
                 cameraMatrices.Dispose();
             }
@@ -51,26 +61,5 @@ namespace Unity.TinyGems
             return inputDeps;
         }
 
-        private static SwapInput GetSwapInputFromWorldPos(ref PhysicsWorld physicsWorld, float2 worldPos, float2 deltaInput)
-        {
-            var pointInput = new OverlapPointInput()
-            {
-                Position = worldPos,
-                Filter = CollisionFilter.Default
-            };
-
-            var swapInput = new SwapInput()
-            {
-                DeltaInput = deltaInput
-            };
-
-            if (physicsWorld.OverlapPoint(pointInput, out var overlapPointHit))
-            {
-                var body = physicsWorld.AllBodies[overlapPointHit.PhysicsBodyIndex];
-                swapInput.Cell = body.Entity;
-            }
-
-            return swapInput;
-        }
     }
 }
